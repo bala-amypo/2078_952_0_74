f7fa0028430581eba6fa39870d92e115a67a715b
bala-amypo <bala@amypo.in>
2025-12-17 09:20:32 +0000

    User commit

--- src/main/java/com/example/demo/config/controller/StudentController.java
+++ src/main/java/com/example/demo/config/controller/StudentController.java
@@ -1,3 +1,14 @@
-import org.springframework.web.bind.annotation.RestController;
-
+package com.example.demo.controller;
+import org.springframework.web.bind.annotation.*;
+import com.example.demo.entity.Student;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.example.demo.Service.StudentService;
 @RestController
+public class StudentController{
+    @Autowired
+    StudentService stdser;
+    @PostMapping("/addStudent")
+    public Student addStudent(@RequestBody Student st){
+        return stdser.poststudent(st);
+    }
+}
--- src/main/java/com/example/demo/service/impl/StudentServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/StudentServiceImpl.java
@@ -1,16 +1,15 @@
-package com.example.demo.service.impl;
-
-import org.springframework.beans.factory.annotation.Autowired;
+package com.example.demo.Service.impl;
 import org.springframework.stereotype.Service;
-
+import com.example.demo.Service.StudentService;
 import com.example.demo.entity.Student;
 import com.example.demo.repository.StudentRepository;
-import com.example.demo.service.StudentService;
-
+import org.springframework.beans.factory.annotation.Autowired;
 @Service
 public class StudentServiceImpl implements StudentService{
+    @Autowired
+    StudentRepository stdrepo;
     @Override
     public Student poststudent(Student st){
-
+            return stdrepo.save(st);
     }
 }
